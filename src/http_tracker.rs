797905bacfb95d191c1e303f0e524af5585f2e51
Bruno Paulino <bruno@bpaulino.com>
2023-02-20 23:42:41 +0100

    Use wiremock for mocking torrent server requests

--- src/http_tracker.rs
+++ src/http_tracker.rs
@@ -1,16 +1,21 @@
 use crate::parser::announce_info::AnnounceInfo;
 use crate::parser::{bencode::BencodeParser, meta_info::Info};
+use reqwest::Client;
 use sha1::{Digest, Sha1};
 
 /// Handle HTTP trackers providing torrent information.
 /// Mostly following the (unofficial) spec from [wiki.theory.org](https://wiki.theory.org/BitTorrentSpecification#Tracker_Request_Parameters)
 pub struct HTTPTracker<'a> {
     peer_id: &'a str,
+    http_client: Client,
 }
 
 impl<'a> HTTPTracker<'a> {
-    pub fn new(peer_id: &'a str) -> Self {
-        Self { peer_id }
+    pub fn new(peer_id: &'a str, http_client: Client) -> Self {
+        Self {
+            peer_id,
+            http_client,
+        }
     }
 
     pub async fn get_announce_info(
@@ -19,12 +24,13 @@ impl<'a> HTTPTracker<'a> {
         info: Info,
     ) -> Result<AnnounceInfo, Box<dyn std::error::Error>> {
         let info_hash = Self::generate_hash(&info.bencode_value);
-        // @TODO: generate a peer ID during client boot?
+        // TODO: generate a peer ID during client boot?
         // Probably read something from the build config and
-        // use some sort of string generator as a suffix
+        // use some sort of string generator as a suffix for each user.
+        // We should persist this so we can reuse the same client ID
+        // between app boots.
         let peer_id = Self::generate_hash(&self.peer_id.as_bytes().to_vec());
 
-        let client = reqwest::Client::new();
         // when using reqwest query methods, the info_hash and peer_id
         // will be URL encoded again, which modifies the binary string.
         // so to keep these query parameters stable, we simply append
@@ -32,7 +38,8 @@ impl<'a> HTTPTracker<'a> {
         // params.
         let url_with_hash = format!("{}?info_hash={}&peer_id={}", url, info_hash, peer_id);
 
-        let response = client
+        let response = self
+            .http_client
             .get(url_with_hash)
             .query(&[
                 ("port", String::from("6889")),
@@ -64,19 +71,34 @@ impl<'a> HTTPTracker<'a> {
 #[cfg(test)]
 mod tests {
 
+    use std::fs;
+
+    use wiremock::ResponseTemplate;
+
     use crate::parser::meta_info::MetaInfo;
 
     use super::*;
 
     #[tokio::test]
-    #[ignore = "Can't test that in CI yet until I have a HTTP client mocking strategy"]
+    // #[ignore = "Can't test that in CI yet until I have a HTTP client mocking strategy"]
     async fn should_get_announce_server_info_from_torrent_file() {
         let meta_info = MetaInfo::from_file("tests/ubuntu_sample.torrent").unwrap();
+        let decoded_announce_response = fs::read("tests/announce_response").unwrap();
+
+        let mock_server = wiremock::MockServer::start().await;
+
+        // Register mock into the mock server
+        wiremock::Mock::given(wiremock::matchers::any())
+            .respond_with(ResponseTemplate::new(200).set_body_bytes(decoded_announce_response))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
         // example of a valid announce URL:
         // https://torrent.ubuntu.com/announce?info_hash=%99%C8%2B%B75%05%A3%C0%B4S%F9%FA%0E%88%1DnZ2%A0%C1&peer_id=%B7%C0%9B%A8%FC%DC%FB%91%C1N%AE%8D%DBZ%E2b%F2%84%B6%E5&port=8888&uploaded=0&downloaded=0&left=555555&compact=1&event=started
-        let http_tracker = HTTPTracker::new("rustorrent-client-dev");
+        let http_tracker = HTTPTracker::new("rustorrent-client-dev", Client::new());
         let resp = http_tracker
-            .get_announce_info(&meta_info.announce, meta_info.info)
+            .get_announce_info(&mock_server.uri(), meta_info.info)
             .await;
 
         assert!(resp.is_ok());
